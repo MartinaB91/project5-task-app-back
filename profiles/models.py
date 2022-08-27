e29ebc947bd493341ca73b8ac275c6ec7e3676fa
MartinaB91 <martinabjorlin@hotmail.com>
2022-08-27 16:11:51 +0000

    feat: AddProfile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,4 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    """
+    The profile is shared between all family members and each member uses same profile for sign in. 
+
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # family_members = models.ForeignKey(FamilyMember, on_delete=models.CASCADE)
