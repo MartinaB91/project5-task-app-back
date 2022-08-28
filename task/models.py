7b009452ef8ae1b35a47acb0898a2d30ff1c53af
MartinaB91 <martinabjorlin@hotmail.com>
2022-08-28 16:13:00 +0000

    feat: Add Tassk model

--- task/models.py
+++ task/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from family_member.models import FamilyMember
 
-# Create your models here.
+
+class Task(models.Model):
+    """
+    Family members are allowed to create a task without a end date
+    """
+    title = models.CharField(max_length=100)
+    creator = models.ForeignKey(
+            FamilyMember,
+            on_delete=models.CASCADE,
+            related_name="task_creator",
+        )
+    # category = models.ForeignKey(
+    #         Category,
+    #         on_delete=models.CASCADE,
+    #         related_name="task_category",
+    #     )
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    end_date = models.DateField(blank=True, null=True)
+    description = models.TextField()
+    star_points = models.IntegerField(default="0")
+    assigned = models.ForeignKey(
+            FamilyMember,
+            on_delete=models.CASCADE,
+            related_name="assigned_family_member",
+            null=True,
+        )
+
