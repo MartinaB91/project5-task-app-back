87ff97d8866bbb21ceb08061e3a226f0ee18fb02
MartinaB91 <martinabjorlin@hotmail.com>
2022-08-29 07:04:52 +0000

    feat: Add str methods to Category and Task models

--- task/models.py
+++ task/models.py
@@ -11,6 +11,9 @@ class Category(models.Model):
     name = models.CharField(max_length=50,  unique=True, blank=False)
     icon = CloudinaryField("category_icon", default="default_image")
 
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Task(models.Model):
     """
@@ -40,4 +43,7 @@ class Task(models.Model):
             related_name="assigned_family_member",
             null=True,
         )
+    
+    def __str__(self):
+        return f"{self.title}"
 
