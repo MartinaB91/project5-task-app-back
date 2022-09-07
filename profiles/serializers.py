e33820aff7e8d4872124f4e84b3e24a8020f331b
MartinaB91 <martinabjorlin@hotmail.com>
2022-09-07 17:36:28 +0000

    feat: Add family_members to ProfileSerializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 from .models import Profile
+from .serializers import FamilyMemberSerializer
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
+    family_members = FamilyMemberSerializer(many=True, read_only=True)
 
     class Meta:
         model = Profile
