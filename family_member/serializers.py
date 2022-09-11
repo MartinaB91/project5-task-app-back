53b80774727a40014f87ae3cbe12ad9145cc064d
MartinaB91 <martinabjorlin@hotmail.com>
2022-09-11 08:05:54 +0000

    feat: Add serializer for serializing family member

--- family_member/serializers.py
+++ family_member/serializers.py
@@ -1,17 +1,33 @@
 from rest_framework import serializers
 from .models import FamilyMember
+from profiles.models import Profile
+# from django.contrib.auth.decorators import login_required
 
 
-class FamilyMember(serializers.ModelSerializer):
-
+# @login_required
+class FamilyMemberSerializer(serializers.ModelSerializer):
+    belongs_to_profile = serializers.StringRelatedField(source='user.profile.id')
+    name = serializers.CharField(max_length=200)
+    #todo: Check why StringRelatedField gives KeyError when CharField work??
+    # family_member_img = serializers.StringRelatedField(source='user.profile.family_member_img.url')
+    family_member_img = serializers.CharField(max_length=400)
+    role = serializers.CharField(max_length=1)
+    
+    def create(self, validated_data):
+        request = self.context.get("request")
+        profile = Profile.objects.filter(user=request.user).first() # Name is unique so it is safe to use first() to get instance instead of QuerySet     
+        return FamilyMember.objects.create(
+            belongs_to_profile=profile,
+            name=validated_data['name'],
+            family_member_img=validated_data['family_member_img'],
+            role=validated_data['role']
+        )
 
     class Meta: 
         model = FamilyMember
-        fields = (
+        fields = [
+            'belongs_to_profile',
             'name',
             'family_member_img',
-            'role',
-            'star_points',
-            'ongoing_tasks',
-            'closed_tasks',
-            )
+            'role'
+        ]
