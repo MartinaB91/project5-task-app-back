df77a8c5691797a0da30b6d6c3de6c7309fcc716
MartinaB91 <martinabjorlin@hotmail.com>
2022-09-15 11:55:42 +0000

    refactor: Removed unused view from profiles serializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -3,19 +3,10 @@ from family_member.models import FamilyMember
 from .models import Profile
 
 
-
-class SimpleFamilyMemberSerializer(serializers.ModelSerializer):
-
-    class Meta: 
-        model = FamilyMember
-        fields = (
-            'name', 'belongs_to_profile',
-            )
-
 class ProfileSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
 
     class Meta:
         model = Profile
-        fields = ('user', 'created_at', 'updated_at',)
+        fields = ['user', 'created_at', 'updated_at',]
 
