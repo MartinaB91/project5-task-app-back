ee43d70234f358ee1f59a8b6b3c9a2f3df450a02
MartinaB91 <martinabjorlin@hotmail.com>
2022-09-15 11:53:02 +0000

    feat: Add detail list for family members

--- family_member/urls.py
+++ family_member/urls.py
@@ -1,9 +1,10 @@
 from  django.urls import path
-from .views import FamilyMemberList
+from .views import FamilyMemberList, familyMemberDetailList
 
 
 urlpatterns = [
     # path('addfamilymember/', CreateFamilyMember.as_view(), name='add_family_member'),
     path('familymembers/', FamilyMemberList.as_view()),
+    path('familymembers/<int:pk>/', familyMemberDetailList.as_view()),
 
 ]
--- family_member/views.py
+++ family_member/views.py
@@ -1,9 +1,50 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from django.http import Http404
+from rest_framework import status
 from rest_framework import generics
 from .models import FamilyMember
 from .serializers import FamilyMemberSerializer
+from rest_framework.response import Response
 
-class FamilyMemberList(generics.ListCreateAPIView):
-    queryset = FamilyMember.objects.all()
+# class FamilyMemberList(generics.ListCreateAPIView):
+#     queryset = FamilyMember.objects.all()
+#     serializer_class = FamilyMemberSerializer
+
+class FamilyMemberList(APIView):
+    """
+    Views all family members
+
+    """
+
+    def get(self, request):
+        family_members = FamilyMember.objects.all()
+        serializer = FamilyMemberSerializer(family_members, many=True)
+        return Response(serializer.data)
+    
+
+class familyMemberDetailList(APIView):
+    """
+    View for displaying and update one family member.  
+    """
     serializer_class = FamilyMemberSerializer
+
+    def get_object(self, pk):
+        try: 
+            family_member = FamilyMember.objects.get(pk=pk)
+            return family_member
+        except FamilyMember.DoesNotExist:
+            raise Http404
+    
+    def get(self,request, pk):
+        family_member = self.get_object(pk)
+        serializer = FamilyMemberSerializer(family_member)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        family_member = self.get_object(pk)
+        serializer = FamilyMemberSerializer(family_member, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
