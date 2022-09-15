34e2b50ed9d41e3162af8cd595d04af2fee9a217
MartinaB91 <martinabjorlin@hotmail.com>
2022-09-15 17:00:13 +0000

    docs: Add comment to permissions func

--- family_star/permissions.py
+++ family_star/permissions.py
@@ -2,6 +2,9 @@ from rest_framework import permissions
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    A profile can only change family members that belongs to that profile. 
+    """
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
