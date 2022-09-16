2438b3de7afe4f1a7e011a267f21fb5afeb53e41
MartinaB91 <martinabjorlin@hotmail.com>
2022-09-16 15:27:30 +0000

    feat: Add serializer for category model

---
+++ categories/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Category
+
+class CategorySerializer(serializers.ModelSerializer):
+    name = serializers.CharField(max_length=50)
+    icon = serializers.ReadOnlyField(source='icon.url')
+
+    class Meta:
+        model = Category
+        fields = [
+            'id',
+            'name',
+            'icon',
+        ]
+
