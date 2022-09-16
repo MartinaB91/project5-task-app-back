00304f9da308cfee86e69af07604a8c1d7056547
MartinaB91 <martinabjorlin@hotmail.com>
2022-09-16 14:55:06 +0000

    refactor: Changed family_members urls

--- family_member/urls.py
+++ family_member/urls.py
@@ -4,7 +4,8 @@ from .views import FamilyMemberList, familyMemberDetailList
 
 urlpatterns = [
     # path('addfamilymember/', CreateFamilyMember.as_view(), name='add_family_member'),
-    path('familymembers/', FamilyMemberList.as_view()),
-    path('familymembers/<int:pk>/', familyMemberDetailList.as_view()),
+    path('members/', FamilyMemberList.as_view()),
+    path('members/<int:pk>/', familyMemberDetailList.as_view()),
 
 ]
+
--- family_star/urls.py
+++ family_star/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
         'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
     ),
     path('', include('profiles.urls')),
-    path('', include('family_member.urls')),
+    path('familymembers/', include('family_member.urls')),
     path('taskboard/', include('task.urls')),
 
 
