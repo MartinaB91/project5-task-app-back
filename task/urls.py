c82d941323946c4e90da2bb13d51edebb680f2ab
MartinaB91 <martinabjorlin@hotmail.com>
2022-09-17 09:10:16 +0000

    feat: Add TaskDetail view

--- task/urls.py
+++ task/urls.py
@@ -1,6 +1,8 @@
 from  django.urls import path
-from .views import TaskListView
+from .views import TaskListView, TaskDetail
+
 
 urlpatterns = [
-    path('tasks/', TaskListView.as_view()), 
+    path('tasks/', TaskListView.as_view()),
+    path('tasks/<int:pk>/', TaskDetail.as_view()), 
 ]
--- task/views.py
+++ task/views.py
@@ -5,13 +5,16 @@ from .models import Task
 from profiles.models import Profile
 from .serializers import TaskSerializer
 from rest_framework.response import Response
+from django.http import Http404
+from family_star.permissions import IsOwner
+
 
 class TaskListView(APIView):
     """
-    Views all tasks that belongs to current profile. 
+    Used for view all tasks that belongs to 
+    current profile and for creating a new task.
     """
     serialzer_class = TaskSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def get(self, request):
         profile = Profile.objects.get(user=request.user)
@@ -29,3 +32,26 @@ class TaskListView(APIView):
             serializer.save(belongs_to_profile=profile)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class TaskDetail(APIView):
+    """
+    Used for displaying one task that belongs to 
+    the current profile, update and delete a task. 
+    """
+    serialzer_class = TaskSerializer
+    permission_classes = [IsOwner]
+
+    def get_object(self, pk):
+        try:
+            task = Task.objects.get(pk=pk)
+            self.check_object_permissions(self.request, task)
+            return task
+        except Task.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk):
+        task = self.get_object(pk)
+        serializer = TaskSerializer(task)
+        return Response(serializer.data)
+
+    
