aef4c7edff92d4391fd1900c1ec525db510f005f
MartinaB91 <martinabjorlin@hotmail.com>
2022-09-30 18:26:25 +0000

    refactor: Restructured if statements in TaskSerializer and added task.done to if/else

--- task/serializers.py
+++ task/serializers.py
@@ -15,7 +15,24 @@ class TaskSerializer(serializers.ModelSerializer):
         Used for updating task form
         """
         task = Task.objects.get(id=instance.id)
-        if validated_data.get('title') is not None:
+        
+        # If payload has no title (only containing info about status) and status id Done. 
+        if validated_data.get('title') is None and validated_data.get('status') is "Done":
+            task.status = "Done"
+
+        # If payload has no title ans status (only containing info about assginment).
+        elif validated_data.get('title') is None and validated_data.get('status') is None:
+            familyMember = FamilyMember.objects.get(name=validated_data.get('assigned'))
+            if task.assigned is not None:
+                task.assigned = None
+                familyMember.ongoing_tasks = familyMember.ongoing_tasks - 1
+            else: 
+                task.assigned = validated_data.get('assigned')
+                familyMember.ongoing_tasks = familyMember.ongoing_tasks + 1 
+
+            familyMember.save()
+
+        else: # Is update in Form
             task.title = validated_data.get('title')
             task.category = Category.objects.get(name=validated_data.get('category')['name'])
             task.end_date = validated_data.get('end_date')
@@ -23,18 +40,9 @@ class TaskSerializer(serializers.ModelSerializer):
             task.star_points = validated_data.get('star_points')
             if (validated_data.get('assigned') is not None):
                 task.assigned = FamilyMember.objects.get(name=validated_data.get('assigned'))
-        if validated_data.get('title') is None:
-            task.assigned = validated_data.get('assigned')
-            familyMember = FamilyMember.objects.get(name=validated_data.get('assigned'))
-            familyMember.ongoing_tasks = familyMember.ongoing_tasks + 1 
-            familyMember.save()
 
         task.save()
         return task
-
-
-        
-
 
     class Meta:
         model = Task
@@ -49,4 +57,5 @@ class TaskSerializer(serializers.ModelSerializer):
             'description',
             'star_points',
             'assigned',
+            'status',
         ]
