b00308b31e24394ff4b80c5d183883fea965e592
MartinaB91 <martinabjorlin@hotmail.com>
2022-10-09 10:22:12 +0000

    feat: Add name to path in category app urls.py

--- categories/urls.py
+++ categories/urls.py
@@ -2,5 +2,5 @@ from  django.urls import path
 from .views import CategoryList
 
 urlpatterns = [
-    path('categories/', CategoryList.as_view()),
+    path('categories/', CategoryList.as_view(), name='categories'),
 ]
