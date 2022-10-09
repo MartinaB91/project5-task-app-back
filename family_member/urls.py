622a53daf0946590a8c627b1ea4e20f3d95693b5
MartinaB91 <martinabjorlin@hotmail.com>
2022-10-09 10:32:18 +0000

    feat: Add names to all paths in family_members urls.py

--- family_member/urls.py
+++ family_member/urls.py
@@ -4,8 +4,8 @@ from .views import FamilyMemberList, FamilyMemberDetailList
 
 urlpatterns = [
     # path('addfamilymember/', CreateFamilyMember.as_view(), name='add_family_member'),
-    path('members/', FamilyMemberList.as_view()),
-    path('members/<int:pk>/', FamilyMemberDetailList.as_view()),
+    path('members/', FamilyMemberList.as_view(), name='family_member_list'),
+    path('members/<int:pk>/', FamilyMemberDetailList.as_view(), name='family_member_detail'),
 
 ]
 
