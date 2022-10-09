4b4656037039dd5754a01240fffb24a23790f580
MartinaB91 <martinabjorlin@hotmail.com>
2022-10-09 10:20:00 +0000

    feat: Add names to all paths in urls.py, task app

--- task/urls.py
+++ task/urls.py
@@ -3,8 +3,8 @@ from .views import TaskListView, TaskDetail, AssignTask, TaskDone
 
 
 urlpatterns = [
-    path('tasks/', TaskListView.as_view()),
-    path('tasks/<int:pk>/', TaskDetail.as_view()), 
-    path('tasks/<int:pk>/assign', AssignTask.as_view()), 
-    path('tasks/<int:pk>/done', TaskDone.as_view()), 
+    path('tasks/', TaskListView.as_view(), name='task_list'),
+    path('tasks/<int:pk>/', TaskDetail.as_view(), name='task'), 
+    path('tasks/<int:pk>/assign', AssignTask.as_view(), name='task_assigned'), 
+    path('tasks/<int:pk>/done', TaskDone.as_view(), name='task_done'), 
 ]
