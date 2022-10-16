e91dcf372d99d39c2bd9e59e248880d397128f87
MartinaB91 <martinabjorlin@hotmail.com>
2022-10-16 16:44:56 +0000

    chore: More cors testing.

--- family_star/settings.py
+++ family_star/settings.py
@@ -51,10 +51,10 @@ if 'DEBUG' in os.environ:
 #     ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
 CORS_ORIGIN_ALLOW_ALL = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["project5-task-app-back.herokuapp.com"]
 
 CORS_ALLOWED_ORIGINS = [
-        '*'
+        'https://project5-task-app-front.herokuapp.com'
     ]
 
 CORS_ALLOW_CREDENTIALS = False
