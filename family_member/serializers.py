51b5f922280b444d7c0e5f21f14b51812829124c
MartinaB91 <martinabjorlin@hotmail.com>
2022-10-16 09:40:45 +0000

    style: Remov commmented out code

--- family_member/serializers.py
+++ family_member/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from .models import FamilyMember
-from profiles.models import Profile
 
 
 class FamilyMemberSerializer(serializers.ModelSerializer):
@@ -8,7 +7,6 @@ class FamilyMemberSerializer(serializers.ModelSerializer):
         source="belongs_to_profile.user.username"
     )
     name = serializers.CharField(max_length=200)
-    # family_member_img = serializers.ReadOnlyField(source='family_member_img.url')
     role = serializers.ChoiceField(choices=FamilyMember.ROLE_PRIVILEGE)
 
     class Meta:
