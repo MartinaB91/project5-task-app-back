4dfa9185e51898de3b3f7d208f6b2fc125ed6ee8
MartinaB91 <martinabjorlin@hotmail.com>
2022-10-17 06:57:53 +0000

    chore: Add default permission classes

--- family_star/settings.py
+++ family_star/settings.py
@@ -88,7 +88,7 @@ SITE_ID = 1
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [(
-        'rest_framework.authentication.SessionAuthentication'
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication'
         # if 'DEV' in os.environ
         # else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
@@ -102,7 +102,11 @@ REST_FRAMEWORK = {
         # else 
         
         #'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
-    )]
+    )],
+
+    'DEFAULT_PERMISSION_CLASSES': [
+    'rest_framework.permissions.IsAuthenticated',
+    ]
 
 }
 
