f00a86affd1117c5022d00024ac6dbbc354a96c6
MartinaB91 <martinabjorlin@hotmail.com>
2022-10-17 06:52:56 +0000

    chore: Test change default authentication classes to session and token

--- family_star/settings.py
+++ family_star/settings.py
@@ -89,8 +89,9 @@ SITE_ID = 1
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [(
         'rest_framework.authentication.SessionAuthentication'
-        if 'DEV' in os.environ
-        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+        'rest_framework.authentication.TokenAuthentication'
+        # if 'DEV' in os.environ
+        # else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
         
 
         # TODO: Check why SessionAuth does not work. User is not signed out on dj-rest-auth/logout request
